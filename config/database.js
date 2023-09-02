da814b47fa0994e18b0070d8aa0901d5bd354a93
rizkyperdananst <riskiperdana345@gmail.com>
2023-09-03 05:11:24 +0700

    Config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const database = mongoose.connect('mongodb://127.0.0.1:27017/student_rest_api').then(() => {
+const database = mongoose.connect('mongodb://127.0.0.1/student_rest_api').then(() => {
     console.log('Database connected');
 }).catch((error) => {
     console.log(`Error: ${error}`);
